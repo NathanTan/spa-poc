dfc78cfcf5a446b91716ea684108f5bf0e3ab21a
Rikki Gibson <rikkigibson@gmail.com>
2016-02-03 17:20:57 -0800

    init with simple jquery datatable demo

---
+++ SPAProofOfConcept/Scripts/app.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="typings/jquery/jquery.d.ts"/>
+/// <reference path="testData.ts"/>
+
+interface BusStop {
+    id: number;
+    name: string;
+    lat: number;
+    lng: number;
+    routeNames: Array<string>;
+}
+
+$(document).ready(() => {
+    $("#content").DataTable({
+        data: testData,
+        columns: [
+            { data: "id", name: "Stop ID" },
+            { data: "name", name: "Stop Name" },
+            { data: "lat", name: "Latitude" },
+            { data: "lng", name: "Longitude" }
+        ]
+    });
+    $("#reverse-button").click(function () {
+        for (var i = 0; i < testData.length; i++) {
+            testData[i].id = parseInt(testData[i].id.toString().split("").reverse().join(""));
+        }
+
+        var dataTable = $("#content").DataTable();
+        dataTable.clear()
+        dataTable.rows.add(testData);
+        dataTable.draw();
+    });
+});
