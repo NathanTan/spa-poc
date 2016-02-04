59c600e8ed2153544e323024b94e0b0c895cf972
Rikki Gibson <rikkigibson@gmail.com>
2016-02-04 11:14:02 -0800

    Toggle paging on and off

--- SPAProofOfConcept/Scripts/app.ts
+++ SPAProofOfConcept/Scripts/app.ts
@@ -1,7 +1,4 @@
-﻿/// <reference path="typings/jquery/jquery.d.ts"/>
-/// <reference path="testData.ts"/>
-
-interface BusStop {
+﻿interface BusStop {
     id: number;
     name: string;
     lat: number;
@@ -9,24 +6,47 @@ interface BusStop {
     routeNames: Array<string>;
 }
 
-$(document).ready(() => {
-    $("#content").DataTable({
+
+function makeDataTable($element: JQuery, usePaging: boolean): GoodDataTable {
+    var options: DataTables.Settings = {
         data: testData,
         columns: [
             { data: "id", name: "Stop ID" },
             { data: "name", name: "Stop Name" },
             { data: "lat", name: "Latitude" },
             { data: "lng", name: "Longitude" }
-        ]
+        ],
+        paging: usePaging
+    };
+    var dataTable = <GoodDataTable>$element.DataTable(options);
+    dataTable.readOnlyOptions = options;
+    return dataTable;
+};
+
+interface GoodDataTable extends DataTables.DataTable {
+    readOnlyOptions: DataTables.Settings;
+}
+
+var tableHTML: string;
+var $dataTable: GoodDataTable;
+$(document).ready(() => {
+    tableHTML = $("#content")[0].outerHTML;
+    $dataTable = makeDataTable($("#content"), true);
+
+    $("#button").click(function () {
+        var isPaging = $dataTable.readOnlyOptions.paging;
+        $dataTable.destroy(true);
+        var $table: JQuery = $(tableHTML).insertAfter("#button");
+        $dataTable = makeDataTable($table, !isPaging);
     });
-    $("#reverse-button").click(function () {
+
+    setInterval(function () {
         for (var i = 0; i < testData.length; i++) {
             testData[i].id = parseInt(testData[i].id.toString().split("").reverse().join(""));
         }
-
-        var dataTable = $("#content").DataTable();
-        dataTable.clear()
-        dataTable.rows.add(testData);
-        dataTable.draw();
-    });
+        $dataTable.clear()
+        $dataTable.rows.add(testData);
+        $dataTable.draw(false);
+        
+    }, 5000);
 });
